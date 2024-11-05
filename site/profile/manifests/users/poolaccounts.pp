3c274cedd00d1c67a0eccf0ec1bebff8e94060e7
kreczko <lkreczko@googlemail.com>
2024-11-05 12:05:16 +0000

    poolaccounts: set defaults for account creation

--- site/profile/manifests/users/poolaccounts.pp
+++ site/profile/manifests/users/poolaccounts.pp
@@ -5,7 +5,17 @@ class profile::users::poolaccounts (
 ) {
   $defaults     = {
     'ensure' => present,
-    'tag'    => 'poolaccounts',
+    'tag'    => 'poolaccounts::groups',
+  }
+  $acc_defaults = {
+    'ensure'       => present,
+    'shell'        => '/sbin/nologin',
+    'password'     => '!!',
+    'create_group' => false,
+    'managehome'   => false,
+    'gid'          => '100',
+    'group'        => 'users',
+    'tag'          => 'poolaccounts::users',
   }
   create_resources('group', $groups, $defaults)
   create_resources('accounts::user', $users, $acc_defaults)
